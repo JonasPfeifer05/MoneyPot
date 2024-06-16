775e1a677ce65acba354ba01c0b3f40a0d8edf30
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-06-17 00:27:21 +0200

    implemented backend

---
+++ backend/index.ts
@@ -0,0 +1,37 @@
+import {kvsLocalStorage} from "@kvs/node-localstorage"
+
+Bun.serve({
+    port: 8080,
+    async fetch(request: Request): Promise<Response> {
+        const storage = await kvsLocalStorage({
+            name: "money-pot",
+            version: 1
+        })
+
+        async function get(): Promise<number> {
+            let amount = await storage.get("amount");
+
+            return amount === undefined ? 0 : amount as number
+        }
+
+        async function set(amount: number) {
+            await storage.set("amount", amount)
+        }
+
+        const url = new URL(request.url);
+        let response;
+        if (url.pathname === "/get") {
+            response = Response.json({amount: await get()})
+        } else if (url.pathname === "/add") {
+            let currentAmount = await get();
+            let addAmount = Number(await request.text())
+            let newAmount = currentAmount - addAmount;
+            await set(newAmount)
+            response = Response.json({amount: newAmount})
+        } else response = new Response("404");
+
+        response.headers.set('Access-Control-Allow-Origin', '*');
+        response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        return response;
+    }
+})
