27801f5efce85067e72e7e9cfe52152e3b80137a
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-06-18 09:15:22 +0200

    reworked backend + added visit count feature

--- backend/index.ts
+++ backend/index.ts
@@ -8,35 +8,53 @@ Bun.serve({
             version: 1
         })
 
-        async function get(): Promise<number[]> {
+        async function getHistory(): Promise<number[]> {
             let amount = await storage.get("amount");
 
             return amount === undefined ? [] : amount as number[]
         }
 
-        async function set(amount: number[]) {
+        async function setHistory(amount: number[]) {
             await storage.set("amount", amount)
+        }
+
+        async function getVisits(): Promise<number> {
+            let visits = await storage.get("visits");
+            return visits == undefined ? 0 : visits as number
+        }
+
+        async function setVisits(visits: number) {
+            await storage.set("visits", visits)
         }
 
         const url = new URL(request.url);
         let response;
-        if (url.pathname === "/") {
+        if (url.pathname === "/" && request.method === "GET") {
             response = new Response(Bun.file(import.meta.dir + "/../html/index.html"))
-        } else if (url.pathname === "/get") {
-            response = Response.json({history: await get()})
-        } else if (url.pathname === "/add") {
-            let current = await get();
+        } else if (url.pathname === "/get" && request.method === "GET") {
+            response = Response.json({history: await getHistory(), visits: await getVisits()})
+        } else if (url.pathname === "/entry" && request.method === "POST") {
+            let current = await getHistory();
             let entry = await request.json()
             let newHistory = [entry, ...current];
-            await set(newHistory)
-            response = Response.json({history: newHistory})
-        } else if (url.pathname === "/delete") {
+            await setHistory(newHistory)
+            response = Response.json({history: newHistory, visits: await getVisits()})
+        } else if (url.pathname === "/entry" && request.method === "DELETE") {
             let index = Number(await request.text())
-            let current = await get();
+            let current = await getHistory();
             current.splice(index, 1)
-            await set(current)
-            console.log(current)
-            response = Response.json({history: current})
+            await setHistory(current)
+            response = Response.json({history: current, visits: await getVisits()})
+        } else if (url.pathname === "/visits" && request.method === "POST") {
+            let current = await getVisits()
+            let newVisits = current + 1
+            await setVisits(newVisits)
+            response = Response.json({history: await getHistory(), visits: newVisits})
+        } else if (url.pathname === "/visits" && request.method === "DELETE") {
+            let current = await getVisits()
+            let newVisits = current - 1
+            await setVisits(newVisits)
+            response = Response.json({history: await getHistory(), visits: newVisits})
         } else {
             response = new Response("404", {status: 404});
         }
